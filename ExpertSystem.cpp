43ab2f454d4ab404b7ade947b35e05f3ec0ae544
Jason_Yuan <47002608+YuhangYuan1@users.noreply.github.com>
2020-02-25 22:24:46 -0600

    Update ExpertSystem.cpp

--- ExpertSystem.cpp
+++ ExpertSystem.cpp
@@ -9,16 +9,17 @@ void backwardChaining()
 {
    map <int, string> knowledgeBase;
 
+   //knowledge base
    knowledgeBase[10] = "science = yes AND designing = yes THEN profession = EGR";
-   knowledgeBase[20] = "science = yes AND designing = yes THEN profession = EGR";
-   knowledgeBase[30] = "science = yes AND designing = yes THEN profession = EGR";
-   knowledgeBase[40] = "science = yes AND designing = yes THEN profession = EGR";
-   knowledgeBase[50] = "science = yes AND designing = yes THEN profession = EGR";
-   knowledgeBase[60] = "science = yes AND designing = yes THEN profession = EGR";
-   knowledgeBase[70] = "science = yes AND designing = yes THEN profession = EGR";
-   knowledgeBase[80] = "science = yes AND designing = yes THEN profession = EGR";
-   knowledgeBase[90] = "science = yes AND designing = yes THEN profession = EGR";
-   knowledgeBase[100] = "science = yes AND designing = yes THEN profession = EGR";
+   knowledgeBase[20] = "science = yes AND experiment = yes THEN profession = SCI";
+   knowledgeBase[30] = "science = yes AND experiment = no THEN profession = MED";
+   knowledgeBase[40] = "science = yes AND nursing = yes THEN profession = HTC";
+   knowledgeBase[50] = "science = no AND art = yes AND communicate = yes AND decisionMaking = yes THEN profession = BUS";
+   knowledgeBase[60] = "science = no AND art = yes AND communicate = yes AND decisionMaking = no THEN profession = COM";
+   knowledgeBase[70] = "science = no AND art = yes AND reading = yes AND writing = yes THEN profession = LBA";
+   knowledgeBase[80] = "science = no AND art = yes AND reading = yes AND writing = no THEN profession = APA";
+   knowledgeBase[90] = "science = no AND art = yes AND reading = no AND crafting = yes THEN profession = FNA";
+   knowledgeBase[100] = "science = no AND art = yes AND grading = yes THEN profession = EDU";
 
    for(int i = 0; i < 10 ; i++)
    {
